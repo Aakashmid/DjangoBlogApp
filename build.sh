053e483d5369559e96ecbd096b66bca6e76ff1a1
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-05-13 22:02:38 +0530

    Updated create superuser opiton

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --noinput 
+fi
+
