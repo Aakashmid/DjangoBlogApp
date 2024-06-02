cdd5913fa3c2e6012325ad007cb823ef49736ae8
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-06-02 20:23:39 +0530

    updated

--- Project_Blog/settings.py
+++ Project_Blog/settings.py
@@ -19,7 +19,7 @@ DEBUG=True if os.environ.get('DEBUG') else True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ['blogblend.onrender.com' ,'127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definitions
@@ -162,5 +162,5 @@ if DEBUG:  # checking whether debug is false or true
     'default': dj_database_url.parse(DB_URL)
     }
 
-    # if os.environ.get('ALLOWED_HOSTS'):
-    #     ALLOWED_HOSTS+=os.environ.get('ALLOWED_HOSTS').split(',')
+    if os.environ.get('ALLOWED_HOSTS'):
+        ALLOWED_HOSTS+=os.environ.get('ALLOWED_HOSTS').split(',')
